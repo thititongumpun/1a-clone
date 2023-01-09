823cd7279cff391709eace4a4c7b03aefa46b033
thititongumpun <thiti180536@gmail.com>
2023-01-09 17:56:52 +0700

    add section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,36 +20,63 @@
 	{/if}
 </div>
 
+<div class="divider" />
+<div class="flex flex-wrap md:flex-row justify-around items-end mb-5 mx-auto">
+	<img src="https://www.thaiamadeus.com/THILA/img/tm.jpg" alt="..." class="w-auto object-contain" />
+	<img
+		src="https://www.thaiamadeus.com/THILA/file/mac_team.gif"
+		alt="..."
+		class="w-auto object-contain"
+	/>
+	<img
+		src="https://www.thaiamadeus.com/THILA/img/checkmytrip.gif"
+		alt="..."
+		class="w-auto object-contain"
+	/>
+	<img
+		src="https://www.thaiamadeus.com/THILA/img/kb_for_travel_agency.gif"
+		alt="..."
+		class="w-auto object-contain"
+	/>
+</div>
+<div class="flex flex-col md:flex-row justify-around">
+	<img src="https://www.thaiamadeus.com/THILA/img/acb.gif" alt="" class="w-auto object-contain" />
+	<img src="https://www.thaiamadeus.com/THILA/img/abs.gif" alt="" class="w-auto object-contain" />
+	<img src="https://www.thaiamadeus.com/THILA/img/abm.gif" alt="" class="w-auto object-contain" />
+	<img src="https://www.thaiamadeus.com/THILA/img/chil.gif" alt="" class="w-auto object-contain" />
+</div>
+<div class="divider" />
+
 <footer class="footer p-10 bg-gray-50 text-neutral-content">
-  <div>
-    <span class="footer-title font-semibold text-blue-500">Our industries</span> 
-    <div class="divider lg:divider-horizontal border"></div> 
-    <a href="/" class="link link-hover text-blue-500">Travel agencies</a>
-    <a href="/" class="link link-hover text-blue-500">Corporations</a>
-    <a href="/" class="link link-hover text-blue-500">Car rental</a>
-    <a href="/" class="link link-hover text-blue-500">Hotels</a>
-  </div> 
-  <div>
-    <span class="footer-title font-semibold text-blue-500">Information for</span> 
-    <div class="divider lg:divider-horizontal border"></div> 
-    <a href="/" class="link link-hover text-blue-500">Business managers</a>
-    <a href="/" class="link link-hover text-blue-500">Sales managers</a>
-    <a href="/" class="link link-hover text-blue-500">IT professionals</a>
-  </div> 
-  <div>
-    <span class="footer-title font-semibold text-blue-500">Why Amadeus</span> 
-    <div class="divider lg:divider-horizontal border"></div> 
-    <a href="/" class="link link-hover text-blue-500">Let's shape the future of travel</a>
-    <a href="/" class="link link-hover text-blue-500">Our ambition</a>
-    <a href="/" class="link link-hover text-blue-500">Our values</a>
-  </div>
-  <div>
-    <span class="footer-title font-semibold text-blue-500">AboUt us</span> 
-    <div class="divider lg:divider-horizontal border"></div> 
-    <a href="/" class="link link-hover text-blue-500">Our company</a>
-    <a href="/" class="link link-hover text-blue-500">Our history</a>
-    <a href="/" class="link link-hover text-blue-500">Our customers</a>
-  </div>
+	<div>
+		<span class="footer-title font-semibold text-blue-500">Our industries</span>
+		<div class="divider divider-horizontal border" />
+		<a href="/" class="link link-hover text-blue-500">Travel agencies</a>
+		<a href="/" class="link link-hover text-blue-500">Corporations</a>
+		<a href="/" class="link link-hover text-blue-500">Car rental</a>
+		<a href="/" class="link link-hover text-blue-500">Hotels</a>
+	</div>
+	<div>
+		<span class="footer-title font-semibold text-blue-500">Information for</span>
+		<div class="divider divider-horizontal border" />
+		<a href="/" class="link link-hover text-blue-500">Business managers</a>
+		<a href="/" class="link link-hover text-blue-500">Sales managers</a>
+		<a href="/" class="link link-hover text-blue-500">IT professionals</a>
+	</div>
+	<div>
+		<span class="footer-title font-semibold text-blue-500">Why Amadeus</span>
+		<div class="divider divider-horizontal border" />
+		<a href="/" class="link link-hover text-blue-500">Let's shape the future of travel</a>
+		<a href="/" class="link link-hover text-blue-500">Our ambition</a>
+		<a href="/" class="link link-hover text-blue-500">Our values</a>
+	</div>
+	<div>
+		<span class="footer-title font-semibold text-blue-500">AboUt us</span>
+		<div class="divider divider-horizontal border" />
+		<a href="/" class="link link-hover text-blue-500">Our company</a>
+		<a href="/" class="link link-hover text-blue-500">Our history</a>
+		<a href="/" class="link link-hover text-blue-500">Our customers</a>
+	</div>
 </footer>
 
 <footer class="footer items-center p-4 bg-gray-100 text-neutral-content justify-center">
