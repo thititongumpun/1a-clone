ba83599e4a48af48a3ffacf42c0f6003f4b477aa
thititongumpun <thiti180536@gmail.com>
2023-01-10 12:24:07 +0700

    add iframe

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,6 +20,42 @@
 	{/if}
 </div>
 
+<!-- <div class="flex justify-center md:justify-around">
+	<div class="container">
+    <p class="bg-rose-700 ">News and events</p>
+  </div>
+	<iframe
+		class="w-1/4 h-1/2 aspect-video "
+		src="https://www.youtube.com/embed/QNapr1JAPms"
+		title="..."
+	/>
+</div> -->
+<div class="flex flex-col md:flex-row justify-between">
+	<div class="flex flex-col">
+		<p class="text-white bg-rose-600 text-left text-lg">News and events</p>
+		<h3 class="text-blue-800 font-semibold">
+			Application Form for Use of Amadeus System <a
+				href="/"
+				class="text-blue-500 hover:text-blue-900">EN</a
+			>
+			/ <a href="/" class="text-blue-500 hover:text-blue-900">TH</a>
+		</h3>
+		<h3 class="text-blue-800 font-semibold">
+			User Management form <a href="/" class="text-blue-500 hover:text-blue-900">EN</a> /
+			<a href="/" class="text-blue-500 hover:text-blue-900">TH</a>
+		</h3>
+		<a href="/" class="text-blue-500 hover:text-blue-900">Welcome to Amadeus Sell connect</a>
+		<a href="/" class="text-blue-500 hover:text-blue-900">Training Schedule</a>
+		<a href="/" class="text-blue-500 hover:text-blue-900">Travel Tools</a>
+	</div>
+  
+	<iframe
+  class="w-screen h-full aspect-video"
+  src="https://www.youtube.com/embed/QNapr1JAPms"
+  title="..."
+/>
+</div>
+
 <div class="divider" />
 <div class="flex flex-wrap md:flex-row justify-around items-end mb-5 mx-auto">
 	<img src="https://www.thaiamadeus.com/THILA/img/tm.jpg" alt="..." class="w-auto object-contain" />
@@ -39,11 +75,28 @@
 		class="w-auto object-contain"
 	/>
 </div>
-<div class="flex flex-col md:flex-row justify-around">
-	<img src="https://www.thaiamadeus.com/THILA/img/acb.gif" alt="" class="w-auto object-contain" />
-	<img src="https://www.thaiamadeus.com/THILA/img/abs.gif" alt="" class="w-auto object-contain" />
-	<img src="https://www.thaiamadeus.com/THILA/img/abm.gif" alt="" class="w-auto object-contain" />
-	<img src="https://www.thaiamadeus.com/THILA/img/chil.gif" alt="" class="w-auto object-contain" />
+
+<div class="flex flex-col md:flex-row flex-wrap justify-around ">
+	<img
+		src="https://www.thaiamadeus.com/THILA/img/acb.gif"
+		alt=""
+		class="w-auto object-scale-down"
+	/>
+	<img
+		src="https://www.thaiamadeus.com/THILA/img/abs.gif"
+		alt=""
+		class="w-auto object-scale-down"
+	/>
+	<img
+		src="https://www.thaiamadeus.com/THILA/img/abm.gif"
+		alt=""
+		class="w-auto object-scale-down"
+	/>
+	<img
+		src="https://www.thaiamadeus.com/THILA/img/chil.gif"
+		alt=""
+		class="w-auto object-scale-down"
+	/>
 </div>
 <div class="divider" />
 
